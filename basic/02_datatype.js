768d7c2da43e4231c7e1f164c353bf6e119a3e90
Man-Mohan-Sharma <manmohan311206@gmail.com>
2025-08-21 17:16:25 +0000

    datatypes

---
+++ basic/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// altert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+let name = "Manmohan"
+let age = 18
+let isLogged = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof"manmohan");  // string
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
