9ff101c034f6ed5fba00ec9a8c520b531ec2f1c7
Man-Mohan-Sharma <manmohan311206@gmail.com>
2025-08-22 19:49:52 +0000

    update

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -12,3 +12,29 @@ console.log(valueInNumber)
 // true => 1 ; false => 0
 // 1 => true; 0 => false
 // "string" => true; ""=> false
+
+// *****************Operations*********
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " Man MOhan Sharma";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); // if string is first the all are treated as string
+console.log(1+2+"2"); // if string is last then first number are caluculated the string 
+
+
+console.log(+true); // will print 1
+console.log(+ "");   // will print 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+3
